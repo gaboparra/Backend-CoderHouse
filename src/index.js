60e1e6415abafd56c80413e35ff6e1b344389038
gaboparra <geparra06@gmail.com>
2023-10-03 16:34:30 -0300

    Reentrega1

--- src/index.js
+++ src/index.js
@@ -4,23 +4,34 @@ class ProductManager {
         this.products = []
     }
 
-    addProduct = ({ title, description, code }) => {
+    static id = 0
 
-        if (this.products.some(product => product.code === code)) return
-        if (!title || !description) return
+    addProduct = ({ title, description, code, price, thumbnail, stock }) => {
 
+        if (this.products.some(product => product.code === code)) return 'El código ya existe, intente con otro.'
+        if (!title || !description || !price || !thumbnail || !stock) return 'Faltan campos a completar'
+        ProductManager.id++
         this.products.push({
             title,
             description,
-            code
+            code,
+            price,
+            thumbnail,
+            stock, 
+            id: ProductManager.id
         })
     }
 
-    getProductById = (code) => {
-        const product = this.products.find(p => p.code === code)
+    getProductById = (id) => {
+       if(!this.products.find((product) => product.id === id)){
+        console.log('No encontrado');
+       }
+       else{
+        console.log(this.products.find((product) => product.id === id));
+       }
 
-        if (product) return product
-        else console.error('No encontrado')
+       // if (product) return product
+       // else console.error('No encontrado')
     }
 }
 
@@ -29,7 +40,10 @@ const productManager = new ProductManager()
 const productToAdd = {
     title: "Mangas",
     description: "",
-    code: "001"
+    code: "001",
+    price: 2000,
+    thumbnail: 'img1',
+    stock: 10
 }
 
 productManager.addProduct(productToAdd)
@@ -37,8 +51,12 @@ productManager.addProduct(productToAdd)
 productManager.addProduct({
     title: "Comics",
     description: "",
-    code: "002"
+    code: "002",
+    price: 2500,
+    thumbnail:'img2',
+    stock: 15
 })
+
 
 console.log(productManager);
 
