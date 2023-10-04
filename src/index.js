4d0524aa265244bf213256f8c229d24ce4c93884
gaboparra <geparra06@gmail.com>
2023-10-04 18:04:23 -0300

    desafío 1

--- src/index.js
+++ src/index.js
@@ -1,62 +1,45 @@
 class ProductManager {
-
     constructor() {
         this.products = []
     }
 
     static id = 0
 
-    addProduct = ({ title, description, code, price, thumbnail, stock }) => {
+    addProduct(title, description, price, thumbnail, code, stock) {
 
         if (this.products.some(product => product.code === code)) return 'El código ya existe, intente con otro.'
         if (!title || !description || !price || !thumbnail || !stock) return 'Faltan campos a completar'
+
         ProductManager.id++
         this.products.push({
             title,
             description,
-            code,
             price,
             thumbnail,
-            stock, 
+            code,
+            stock,
             id: ProductManager.id
         })
     }
+    getProduct() {
+        return this.products;
+    }
 
-    getProductById = (id) => {
-       if(!this.products.find((product) => product.id === id)){
-        console.log('No encontrado');
-       }
-       else{
-        console.log(this.products.find((product) => product.id === id));
-       }
-
-       // if (product) return product
-       // else console.error('No encontrado')
+    getProductById(id) {
+        if (!this.products.find((product) => product.id === id)) {
+            console.log('No encontrado');
+        } else {
+            console.log(this.products.find((product) => product.id === id));
+        }
     }
 }
 
-const productManager = new ProductManager()
+const products = new ProductManager();
 
-const productToAdd = {
-    title: "Mangas",
-    description: "",
-    code: "001",
-    price: 2000,
-    thumbnail: 'img1',
-    stock: 10
-}
+products.addProduct('title1', 'description1', 2000, 'img1', '001', 10)
+products.addProduct('title2', 'description2', 2500, 'img2', '002', 15)
 
-productManager.addProduct(productToAdd)
+console.log(products.getProduct());
 
-productManager.addProduct({
-    title: "Comics",
-    description: "",
-    code: "002",
-    price: 2500,
-    thumbnail:'img2',
-    stock: 15
-})
-
-
-console.log(productManager);
+(products.getProductById(1));
 
